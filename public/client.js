58cff3c555ef7911c79ea2c3b02a8f616b8670cb
unknown <bamtorrr@gmail.com>
2023-12-04 20:00:50 +0900

    things I worked on

---
+++ public/client.js
@@ -0,0 +1,83 @@
+const SENTIMENT = {
+  negative: '부정적',
+  positive: '긍정적',
+  neutral: '중립적',
+};
+
+const targetName = document.getElementById('targetName');
+const targetedName = document.getElementById('targetedName');
+const comment = document.getElementById('comment');
+const submitButton = document.getElementById('post');
+const analysisResult = document.getElementById('analysisResult');
+const imgPosition = document.getElementById('imgPosition');
+const container = document.getElementById('container');
+const resultContainer = document.getElementById('resultContainer');
+
+const optionsForm = (body) => ({
+  method: 'POST',
+  body: JSON.stringify(body),
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const paintResult = (res) => {
+  resultContainer.classList.remove('hidden');
+
+  const result = res.document;
+  const totalResult = result.sentiment;
+
+  analysisResult.textContent = SENTIMENT[totalResult];
+  targetedName.textContent = targetName.value;
+
+  const bgImage = document.createElement('img');
+  bgImage.src = `${totalResult}.svg`;
+  imgPosition.appendChild(bgImage);
+
+  Object.entries(result.confidence).map((satus) => {
+    let bgColor;
+    if (satus[0] === 'negative') {
+      bgColor = 'bg-rose-600';
+    } else if (satus[0] === 'positive') {
+      bgColor = 'bg-sky-500';
+    } else {
+      bgColor = 'bg-zinc-500';
+    }
+    const rate = document.createElement('p');
+    const divEl = document.createElement('div');
+    const spanEl = document.createElement('p');
+
+    divEl.className = 'bg-zinc-50 h-4 rounded-full w-full mb-1 flex';
+    rate.className = 'px-1 py-1 rounded-lg text-gray-600 text-xs mb-3';
+    spanEl.className = `h-full ${bgColor} w-full block rounded-full`;
+
+    rate.textContent = `${SENTIMENT[satus[0]]} : ${Math.round(satus[1])}%`;
+    spanEl.style.width = `${Math.round(satus[1])}%`;
+
+    container.appendChild(divEl);
+    divEl.appendChild(spanEl);
+    container.appendChild(rate);
+
+    return null;
+  });
+};
+
+const fetchAPI = async (content) => {
+  const url = '/submitComment';
+  const body = {
+    content,
+  };
+
+  await fetch(url, optionsForm(body))
+    .then((response) => response.json())
+    .then((response) => paintResult(response))
+    .catch((e) => console.error(e));
+};
+
+const submitAnalysis = () => {
+  if (targetName.value !== '' || comment.value !== '') {
+    fetchAPI(comment.value);
+  }
+};
+
+submitButton.addEventListener('click', submitAnalysis);
---
+++ server.js
@@ -0,0 +1,38 @@
+const dotenv = require('dotenv');
+
+const express = require('express');
+
+const request = require('request');
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => res.sendFile('index.html'));
+
+// localhost:3000/submitComment로 요청 시 동작할 핸들러
+app.post('/submitComment', (req, res) => {
+  const url = 'https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze';
+
+  const options = {
+    url,
+    body: JSON.stringify(req.body),
+    headers: {
+      'Content-Type': 'application/json',
+      'X-NCP-APIGW-API-KEY-ID': process.env.CLIENT_ID,
+      'X-NCP-APIGW-API-KEY': process.env.CLIENT_SECRET_ID,
+    },
+  };
+
+  request.post(options, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      res.send(body);
+    }
+  });
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`http://localhost:${port}/ app listening on port ${3000}`));
